bb6ebd5cce065a3cda8ecff96e751ee24a00cdc7
BhanuSaiTejaMarisa <bhanu3475@gmail.com>
2021-09-04 11:55:37 +0530

    streaming option

--- logic.js
+++ logic.js
@@ -1,16 +1,33 @@
 //console.log("s")
 
 var container=document.getElementById("container");
-var bars=[]
-for(let i=0;i<10;i++){
-    bars[i]=document.createElement("div");
-    bars[i].style.height=`${1+Math.floor(99*Math.random())}px`;
-    container.appendChild(bars[i])
-}
-
+var bars=[];
 var  count=0;
  //console.log(bars);
 var timer;
+
+for(let i=0;i<10;i++){
+    bars[i]=document.createElement("div");
+    bars[i].style.height=`${1+Math.floor(99*Math.random())}px`;;
+    bars[i].setAttribute("id",`${i}-id`);
+    bars[i].addEventListener("click",(e)=>{
+        console.log(e.target.id);
+        var end=+e.target.id.split("-")[0];
+        if(count>end){
+            for(let i=end+1;i<=count;i++){  //end->end+1 
+                bars[i].style.backgroundColor="inherit"
+            }
+        }
+        else{
+            for(let i=0;i<=end;i++){
+                bars[i].style.backgroundColor="black"
+            }
+
+        }
+        count=end;
+    })
+    container.appendChild(bars[i])
+}
 
 function Start() {
     if(start.textContent=="play"){
@@ -21,7 +38,7 @@ function Start() {
 
                  bars[count].style.backgroundColor="black"
              }
-            //console.log(count);
+            console.log(count,"count");
             
             if(count==10){
                 for(let i=0;i<bars.length;i++){
@@ -40,8 +57,9 @@ function Start() {
 
 }
 var start=document.createElement("button");
-start.textContent="play"
-document.getElementById("play").appendChild(start)
+start.textContent="play";
+var playDiv=document.getElementById("play")
+playDiv.children[0].appendChild(start)
 start.addEventListener("click",()=>{
     Start()
 })
