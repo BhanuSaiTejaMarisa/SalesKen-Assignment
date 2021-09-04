b0732863fd4a669017eb4f2be067fb69a63d5387
BhanuSaiTejaMarisa <bhanu3475@gmail.com>
2021-09-04 16:38:51 +0530

    canvas play/pause

--- logic.js
+++ logic.js
@@ -63,3 +63,103 @@ playDiv.children[0].appendChild(start)
 start.addEventListener("click",()=>{
     Start()
 })
+
+var canvas=document.querySelector("canvas");
+canvas.width=800;
+canvas.height=300;
+var context=canvas.getContext('2d')
+console.log(context);
+var x=10,y=100;
+var width=10,height=50
+var rect=[];
+var offset=5;
+function Rectangle(x,y,width,height){
+    this.x=x;
+    this.y=y;
+    this.width=width;
+    this.height=height;
+    this.create=()=>{
+        context.strokeStyle="green";
+        context.strokeRect(this.x,this.y,this.width,this.height);
+        
+        context.fillStyle=`yellow`
+        context.fillRect(this.x,this.y,this.width,this.height);
+      
+    }
+    this.style=()=>{
+        context.fillStyle="red"
+        context.fillRect(this.x,this.y,this.width,this.height);
+    }
+    this.reset=()=>{
+        context.fillStyle="yellow"
+        context.fillRect(this.x,this.y,this.width,this.height);
+    }
+}
+
+for(let i=1;i<=50;i++){
+    //rect[i-1]=
+    //context.fillStyle=`rgb(${Math.floor(255*Math.random())},${Math.floor(255*Math.random())},${Math.floor(255*Math.random())})`
+    rect[i-1]=new Rectangle(x+offset,y-Math.floor(50*Math.random()),width,height+Math.floor(50*Math.random()))
+    rect[i-1].create()
+    offset+=15
+}
+var move=0;
+var canvasTimer
+function animate(){
+    //requestAnimationFrame(animate);
+    //console.log("f");
+    if(canvasButton.textContent=="play"){
+        canvasButton.textContent="pause"
+        canvasTimer= setInterval(()=>{
+        rect[move].style()
+        move++;
+        if(move==50){
+            move=0;
+            canvasButton.textContent="play";
+            clearInterval(canvasTimer);
+            reset()
+        }
+        console.log(move);
+    },500)
+    
+    }
+    else{
+        canvasButton.textContent="play";
+        clearInterval(canvasTimer)
+    }   
+   //context.fillRect(x+offset,y-Math.floor(50*Math.random()),width,height+Math.floor(50*Math.random()))
+   
+}
+
+//animate()
+//console.log(rect);
+var canvasButton=document.createElement("button");
+canvasButton.textContent="play"
+document.querySelector("body").appendChild(canvasButton)
+canvasButton.addEventListener("click",()=>{
+    animate()
+})
+
+function reset(){
+    for(let i=0;i<50;i++){
+        rect[i].reset()
+    }
+}
+canvas.addEventListener("click",(e)=>{
+    console.log("click",e.target,e.clientX,e.clientY);
+})
+//doesn't work
+// rect[0].addEventListener("click",()=>{
+//     console.log(0);
+// })
+
+
+// console.log(context);
+// console.log(canvas);
+
+//dummy=context.fillRect(0,0,100,200);
+
+// context.beginPath()
+// context.stroke()
+
+// context.fill()
