35771d47a411d7bccb806397a7d44d42cf2068ba
BhanuSaiTejaMarisa <bhanu3475@gmail.com>
2021-09-05 15:25:42 +0530

    added comments

--- logic.js
+++ logic.js
@@ -74,25 +74,31 @@ start.addEventListener("click",()=>{
 //CANVAS
 
 var canvas=document.querySelector("canvas");
+
 canvas.width=780;
 canvas.height=200;
+
 var context=canvas.getContext('2d')
 //console.log(context);
 
 var x=10,y=100;
-var width=10,height=50
-var rect=[];
+var width=10,height=50;
 var offset=5;
 var pointer=0;
 
+var rect=[];                                                            //contains all bars
 
-var stack=[]
+var stack=[]                                                            //contains pointer lines
+
+
 //Constructor
 function Rectangle(x,y,width,height){
     this.x=x;
     this.y=y;
     this.width=width;
     this.height=height;
+
+    //Creates all bars
     this.create=()=>{
         context.strokeStyle="green";
         context.strokeRect(this.x,this.y,this.width,this.height);
@@ -101,21 +107,25 @@ function Rectangle(x,y,width,height){
         context.fillRect(this.x,this.y,this.width,this.height);
       
     }
+
+    //Fills bars with red
     this.style=()=>{
         context.fillStyle="red"
         context.fillRect(this.x,this.y,this.width,this.height);
     }
+
+    //Resets bars to yellow
     this.reset=()=>{
         context.fillStyle="yellow"
-        context.fillRect(this.x,this.y,this.width,this.height);
-      //  pointer=0;
+        context.fillRect(this.x,this.y,this.width,this.height);   
     }
+
     this.boundary=(X,Y,i)=>{
         this.reset()
         if(this.x+7.5<=X){
             // console.log(true,this.x,X,this.y,Y);
             // console.log(i);
-            stack.push(i)
+            //stack.push(i)
             context.fillStyle="red"
             context.fillRect(this.x,this.y,this.width,this.height);
             //console.log(i);
@@ -124,7 +134,7 @@ function Rectangle(x,y,width,height){
     }
 }
 
-//Creation
+//Creation phase 
 for(let i=1;i<=50;i++){
   
     rect[i-1]=new Rectangle(x+offset,y-Math.floor(50*Math.random()),width,height+Math.floor(50*Math.random()))
@@ -141,30 +151,33 @@ function animate(){
     if(canvasButton.textContent=="play"){
         canvasButton.textContent="pause"
         canvasTimer= setInterval(()=>{
-        rect[pointer].style()
-        pointer++;
+            if(pointer<=49){
+                rect[pointer].style()
+            }
         if(pointer==50){
             pointer=0;
             canvasButton.textContent="play";
             clearInterval(canvasTimer);
             reset()
         }
+        pointer++;
         //console.log(pointer);
-    },500)
+    },250)
     
     }
     else{
         canvasButton.textContent="play";
         clearInterval(canvasTimer)
     }   
-   //context.fillRect(x+offset,y-Math.floor(50*Math.random()),width,height+Math.floor(50*Math.random()))
    
 }
-
-//console.log(rect);
+// Play/Pause button
 var canvasButton=document.createElement("button");
-canvasButton.textContent="play"
-document.querySelector("body").appendChild(canvasButton)
+canvasButton.textContent="play";
+document.querySelector("body").appendChild(canvasButton);
+
+
+// Button onclick streams bars to red
 canvasButton.addEventListener("click",()=>{
     animate()
 })
@@ -182,7 +195,7 @@ canvas.addEventListener("click",(e)=>{
     
         //console.log("click fn");
         barClick(e.clientX,e.clientY)
-        flagDoubleClick=0;
+        //flagDoubleClick=0;
         canvas.removeEventListener("mousemove",streamPointer)
 })
 
@@ -192,7 +205,10 @@ function barClick(X,Y){
     }
     //console.log(pointer);
 }
+
 var lineStack=[]
+
+//Mouse pointer on top left corner of canvas 
 context.strokeStyle="purple";
 context.strokeRect(0,0,20,20)
 context.moveTo(5,4)
@@ -206,9 +222,11 @@ context.lineTo(16,12);
 context.lineTo(11,8);
 context.stroke();
 
-var flagDoubleClick=0
+var flagDoubleClick=0;
+
+//Adds streaming option on double clicking the pointer
 canvas.addEventListener("dblclick",(e)=>{
-    //console.log("dblcick",flagDoubleClick);
+    
     if(e.x<28&&e.y<260&&flagDoubleClick==0){
 
         canvas.addEventListener("mousemove",streamPointer)
@@ -220,53 +238,47 @@ canvas.addEventListener("dblclick",(e)=>{
         canvas.removeEventListener("mousemove",streamPointer)
     }
     //console.log("dblcick",flagDoubleClick);
-
 })
 
 
-// pointer for streaming
+// Pointer function for streaming
 var streamPointer=(e)=>{
+
     if(lineStack.length!=0){
         for(let j=0;j<lineStack.length-1;j++){
             var prevlineX=lineStack[j]
             lineStack.shift()
+           
             context.clearRect(prevlineX,0,2,200);
-        }
-        // context.strokeStyle="black";
-        // context.moveTo(e.x-8,0);
-        // context.lineTo(e.x-8,200);
-        // context.stroke();
-
-       
+        } 
     }
+
     for(let i=0;i<50;i++){
         rect[i].create()
     }
-    //console.log(e.x,e.y);
+    
     if(e.x>30){
         context.fillStyle="green";
         context.fillRect(e.x-8,0,2, 200);
         lineStack.push(e.x-8);
         barClick(e.clientX,e.clientY)
     }
-tags()
-  
-    // context.lineTo(e.x-8,200);
-    // context.();
-    //console.log("hover");
-    //console.log(lineStack);
+
+    tags()
 }
 
 function tags(){
     context.strokeStyle="orange";
+    context.beginPath()
     context.moveTo(20,rect[0].y);
     context.lineTo(20,35);
+    context.stroke();
     context.strokeRect(20,20,90,20)
     // console.log(rect[0]);
     context.fillStyle="orange"
     context.font="15px Georgia"
     context.fillText("Introduction",20, 35);
-    context.stroke();
+   // context.fill()
 
     context.strokeStyle="blue";
     context.moveTo(362,rect[23].y);
@@ -283,7 +295,7 @@ function tags(){
     context.lineTo(750,35);
     context.strokeRect(660,20,90,20)
     // console.log(rect[0]);
-    context.fillStyle="blue"
+    context.fillStyle="teal"
     context.font="15px Georgia"
     context.fillText(" Conclusion",660, 35);
     context.stroke()
